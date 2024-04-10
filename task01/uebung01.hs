c1aeb4c105e42b9aa201036b5466bbb0f6f370c7
Florian Will <florian.will01@stud.uni-goettingen.de>
2024-04-10 21:26:57 +0200

    task01 fertig

--- task01/uebung01.hs
+++ task01/uebung01.hs
@@ -1,3 +1,5 @@
+import Data.Char (toLower)
+
 quadratic :: (Int , Int , Int ) -> Int -> Int
 quadratic (a, b, c) x = a * x * x + b * x + c
 
@@ -12,3 +14,27 @@ sumList [] = 0
 sumList (x:xs) = x + sumList xs
 
 foldList :: ( Double -> Double -> Double ) -> [ Double ] -> Double
+foldList f[x] = x
+foldList f(x:xs) = f x (foldList f xs)
+
+mapList :: ( Int -> Int ) -> [ Int ] -> [ Int ]
+mapList f [] = []
+mapList f (x:xs) = f x : mapList f xs
+
+tableInt :: (Int -> Int) -> [Int] -> String
+tableInt f [] = ""
+tableInt f [x] = show x ++ ":" ++ show (f x)
+tableInt f (x:xs) = show x ++":"++ show (f x) ++ "\n" ++ tableInt f xs
+
+
+containsList :: [ Int ] -> Int -> Bool
+containsList [] _ = False
+containsList (x:xs) n | x == n = True
+                      | otherwise = containsList xs n
+
+countList :: [Char] -> Char -> Int
+countList [] _ = 0
+countList (x:xs) n | toLower x == n = 1 + countList xs n
+                   | otherwise = countList xs n
+
+
